8f164182a68650cfa86061a2fb45336611ff83cb
hml528528 <69610852+hml528528@users.noreply.github.com>
2020-10-06 16:16:25 +0800

    to local
    to local

--- src/cn/java/demo/testGithub.java
+++ src/cn/java/demo/testGithub.java
@@ -20,6 +20,7 @@ package cn.java.demo;
 public class testGithub {
     public static void main(String[] args) {
         System.out.println("hello,github01");
+        System.out.println("hello,github02");
     }
 
 }
