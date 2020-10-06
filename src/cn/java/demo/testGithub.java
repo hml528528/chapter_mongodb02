98b05119a4b41c27f6d36fb5046d6f9d09914c23
hml528528 <78174337@qq.com>
2020-10-06 16:14:42 +0800

    create file 02

---
+++ src/cn/java/demo/testGithub.java
@@ -0,0 +1,25 @@
+/**
+ * Project Name:chapter_mongodb02
+ * File Name:testGithub.java
+ * Package Name:cn.java.demo
+ * Date:2020年10月6日下午4:13:31
+ * Copyright (c) 2020, bluemobi All Rights Reserved.
+ *
+*/
+
+package cn.java.demo;
+
+/**
+ * Description: github测试 <br/>
+ * Date: 2020年10月6日 下午4:13:31 <br/>
+ * 
+ * @author HML
+ * @version
+ * @see
+ */
+public class testGithub {
+    public static void main(String[] args) {
+        System.out.println("hello,github01");
+    }
+
+}
